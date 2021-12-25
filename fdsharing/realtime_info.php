2e24f55a31995241e9408b88badb8f9225122939
KEVIN <77971017+keivnsyu@users.noreply.github.com>
2021-12-25 12:19:12 +0800

    Update realtime_info.php

--- fdsharing/realtime_info.php
+++ fdsharing/realtime_info.php
@@ -177,7 +177,7 @@ $picture = $row_realtime['picture'];
 			  if ($address != NULL){
 			?>
 			<div class="video-container">
- <iframe  width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=<?php  echo $address; ?>&key=AIzaSyB6rPWjbHyRZoomestCID_UVGx27rHMqW8" allowfullscreen >d  </iframe>
+ <iframe  width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=<?php  echo $address; ?>&key=Your Keys" allowfullscreen >d  </iframe>
 </div>
 			  <?php }else{
 				  echo "error";
